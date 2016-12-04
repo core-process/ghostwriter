76c7de384837c6823a53c6b5f3a312211ab61622
Niklas Salmoukas <niklas@salmoukas.com>
2016-12-05 00:59:05 +0100

    add publish option to build scripts

--- build.sh
+++ build.sh
@@ -2,23 +2,45 @@
 set -e
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ENABLE_PUBLISH=false
+
+for i in "$@"
+do
+  case $i in
+    -p|--publish)
+      ENABLE_PUBLISH=true
+    ;;
+    *)
+    ;;
+  esac
+done
 
 cd "$ROOT_DIR/common"
 npm run build
-npm publish
+if [ "$ENABLE_PUBLISH" = true ]; then
+  npm publish
+fi
 
 cd "$ROOT_DIR/apptool"
 npm run build
-npm publish
+if [ "$ENABLE_PUBLISH" = true ]; then
+  npm publish
+fi
 
 cd "$ROOT_DIR/middleware"
 npm run build
-npm publish
+if [ "$ENABLE_PUBLISH" = true ]; then
+  npm publish
+fi
 
 cd "$ROOT_DIR/service"
 npm run build
-npm publish
-./build-image.sh
+if [ "$ENABLE_PUBLISH" = true ]; then
+  npm publish
+  ./build-image.sh --publish
+else
+  ./build-image.sh
+fi
 
 cd "$ROOT_DIR"
 echo "Done!"

a3d6c763dd92dc1765f2deaee2445b988f7a478c
Niklas Salmoukas <niklas@salmoukas.com>
2016-12-05 01:46:21 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -2,11 +2,20 @@
 set -e
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 ENABLE_PUBLISH=false
+ENABLE_DOCKER=false
+ENABLE_REBUILD=false
 
 for i in "$@"
 do
   case $i in
+    -d|--docker)
+      ENABLE_DOCKER=true
+    ;;
+    -r|--rebuild)
+      ENABLE_REBUILD=true
+    ;;
     -p|--publish)
       ENABLE_PUBLISH=true
     ;;
@@ -16,37 +25,55 @@ do
 done
 
 cd "$ROOT_DIR/common"
-rm -r -f build
+if [ "$ENABLE_REBUILD" = true ]; then
+  rm -r -f node_modules build
+  npm install
+fi
 npm run build
 if [ "$ENABLE_PUBLISH" = true ]; then
   npm publish
 fi
 
 cd "$ROOT_DIR/apptool"
-rm -r -f build
+if [ "$ENABLE_REBUILD" = true ]; then
+  rm -r -f node_modules build
+  npm install
+fi
 npm run build
 if [ "$ENABLE_PUBLISH" = true ]; then
   npm publish
 fi
 
 cd "$ROOT_DIR/middleware"
-rm -r -f build
+if [ "$ENABLE_REBUILD" = true ]; then
+  rm -r -f node_modules build
+  mkdir node_modules
+  ln -s ../../common node_modules/ghostwriter-common
+  npm install
+fi
 npm run build
 if [ "$ENABLE_PUBLISH" = true ]; then
   npm publish
 fi
 
 cd "$ROOT_DIR/service"
-rm -r -f build
+if [ "$ENABLE_REBUILD" = true ]; then
+  rm -r -f node_modules build
+  mkdir node_modules
+  ln -s ../../common node_modules/ghostwriter-common
+  npm install
+fi
 npm run build
 if [ "$ENABLE_PUBLISH" = true ]; then
   npm publish
 fi
 
-cd "$ROOT_DIR"
-docker build -f service/Dockerfile -t quay.io/process_team/ghostwriter-service:latest --no-cache ./
-if [ "$ENABLE_PUBLISH" = true ]; then
-  docker push quay.io/process_team/ghostwriter-service:latest
+if [ "$ENABLE_DOCKER" = true ]; then
+  cd "$ROOT_DIR"
+  docker build -f service/Dockerfile -t quay.io/process_team/ghostwriter-service:latest --no-cache ./
+  if [ "$ENABLE_PUBLISH" = true ]; then
+    docker push quay.io/process_team/ghostwriter-service:latest
+  fi
 fi
 
 cd "$ROOT_DIR"
